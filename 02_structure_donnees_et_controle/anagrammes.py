ea169ad40f08fc90165c39c55f44ee86bd94529b
Rafael Shroom <68326435+shroom-r@users.noreply.github.com>
2025-02-03 22:14:51 +0100

    Exercice 02 structure donnees et controle (correction anagrammes.py)

--- 02_structure_donnees_et_controle/anagrammes.py
+++ 02_structure_donnees_et_controle/anagrammes.py
@@ -1,12 +1,33 @@
+import re
+import unicodedata
+
 words = [w.strip() for w in open('french_words.txt', encoding='utf-8')]
 
 
 inputWord = input("Entrez un mot : ").lower()
 while len(inputWord)>0:
-    sortedInput = sorted(inputWord)
+    normalizedInput = inputWord
+    # Replace accents from input with non accented chars
+    normalizedInput = unicodedata.normalize('NFKD', normalizedInput)
+    normalizedInput = ''.join([c for c in normalizedInput if not unicodedata.combining(c)])
+
+    # Remove all non letter chars from input
+    normalizedInput = re.sub(r'[^a-zA-ZÀ-ÖØ-öø-ÿ]','',normalizedInput)
+
+    normalizedInput = "".join(sorted(normalizedInput))
 
     for word in words:
-        if (sorted(word.lower()) == sortedInput):
+        normalizedWord = word
+        # Replace accents from word with non accented chars
+        normalizedWord = unicodedata.normalize('NFKD', normalizedWord)
+        normalizedWord = ''.join([c for c in normalizedWord if not unicodedata.combining(c)])
+        # Remove all non letter chars from input
+        normalizedWord = re.sub(r'[^a-zA-ZÀ-ÖØ-öø-ÿ]','',normalizedWord)
+
+        normalizedWord = "".join(sorted(normalizedWord))
+
+        # Clean and normalize word
+        if (normalizedWord == normalizedInput):
             print(word)
     
     inputWord = input("Entrez un mot : ").lower()
