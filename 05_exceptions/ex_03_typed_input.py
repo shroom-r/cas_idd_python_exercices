7d978e5a2fbb9e636da830db0ecb94efaa743eed
Rafael Shroom <68326435+shroom-r@users.noreply.github.com>
2025-03-06 19:15:38 +0100

    Exercice 05 exceptions quelques corrections

--- 05_exceptions/ex_03_typed_input.py
+++ 05_exceptions/ex_03_typed_input.py
@@ -1,11 +1,27 @@
+# def typed_input(prompt, expectedType = int):
+#     value = input(prompt)
+#     try:
+#         convertedValue = expectedType(value)
+#         if (str(value) != str(convertedValue)):
+#             raise
+#     except:
+#         print("Vous devez fournir une entrée du type: " + str(expectedType))
+#         typed_input(prompt, expectedType)
+#     else :
+#         print(convertedValue)
+
+
+# Version corrigée. Le fait d'appeler typed_input dans l'exception crée un stack d'appel potentiellement croissant à l'infini. Tant dis que le mettre dans une boucle non
 def typed_input(prompt, expectedType = int):
-    value = input(prompt)
-    try:
-        convertedValue = expectedType(value)
-        if (str(value) != str(convertedValue)):
-            raise
-    except:
-        print("Vous devez fournir une entrée du type: " + str(expectedType))
-        typed_input(prompt, expectedType)
-    else :
-        print(convertedValue)
+
+    while True:
+        value = input(prompt)
+        try:
+            convertedValue = expectedType(value)
+            if (str(value) != str(convertedValue)):
+                raise
+        except:
+            print("Vous devez fournir une entrée du type: " + str(expectedType))
+        else :
+            print(convertedValue)
+            return convertedValue
--- 05_exceptions/ex_04_roulette.py
+++ 05_exceptions/ex_04_roulette.py
@@ -1,24 +1,42 @@
-counter1 = 9
-counter2 = 10
-counter3 = 11
+# counter1 = 9
+# counter2 = 10
+# counter3 = 11
+# try:
+#     while True:
+#         if counter1 == 9:
+#             counter1 = 0
+#         else:
+#             counter1 += 1
+#         if counter2 == 10:
+#             counter2 = 0
+#         else:
+#             counter2 += 1
+#         if counter3 == 11:
+#             counter3 = 0
+#         else:
+#             counter3 += 1
+# except KeyboardInterrupt:
+#     print("Vous avez appuyé ctrl-c!")
+#     print(f'Valeurs de compteurs : {counter1} {counter2} {counter3}')
+#     if counter1 == counter2 and counter2 == counter3:
+#         print("Vous avez gagné !")
+#     else:
+#         print("Vous avez perdu !")
+
+# Correction, bien meilleure version
+x = y = z = 0
+
 try:
     while True:
-        if counter1 == 9:
-            counter1 = 0
-        else:
-            counter1 += 1
-        if counter2 == 10:
-            counter2 = 0
-        else:
-            counter2 += 1
-        if counter3 == 11:
-            counter3 = 0
-        else:
-            counter3 += 1
+        x = (x+1) % 10
+        y = (x+1) % 11
+        z = (x+1) % 12
 except KeyboardInterrupt:
-    print("Vous avez appuyé ctrl-c!")
-    print(f'Valeurs de compteurs : {counter1} {counter2} {counter3}')
-    if counter1 == counter2 and counter2 == counter3:
-        print("Vous avez gagné !")
-    else:
-        print("Vous avez perdu !")
+    print('Vous avez appuyé ctrl-c!')
+
+print('Valeurs des compteurs:', x, y, z)
+
+if x == y == z:
+    print("Vous avez gagné!")
+else:
+    print("Vous avez perdu!")
